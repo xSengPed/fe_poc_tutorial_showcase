0489b519144beccc06ab1c5301c0026b44b84af6
Donnukrit Satirakul <kalima007c@gmail.com>
2026-03-05 20:33:54 +0700

    added quick action button

--- lib/ui/views/landing_page/landing_page.dart
+++ lib/ui/views/landing_page/landing_page.dart
@@ -1,3 +1,4 @@
+import 'package:fe_poc_tutorial_showcase/ui/widgets/quick_action_panel.dart';
 import 'package:flutter/material.dart';
 
 class LandingPage extends StatefulWidget {
@@ -10,6 +11,15 @@ class LandingPage extends StatefulWidget {
 class _LandingPageState extends State<LandingPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            spacing: 8,
+            children: [const SizedBox(height: 16), QuickActionPanel()],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/ui/widgets/quick_action_panel.dart
+++ lib/ui/widgets/quick_action_panel.dart
@@ -3,8 +3,44 @@ import 'package:flutter/material.dart';
 class QuickActionPanel extends StatelessWidget {
   const QuickActionPanel({super.key});
 
+  GestureDetector _createQuickActionButton({
+    String title = '',
+    Icon icon = const Icon(Icons.question_mark_outlined),
+    VoidCallbackAction? onTap,
+  }) {
+    return GestureDetector(
+      onTap: () {
+        onTap;
+      },
+      child: Column(
+        mainAxisSize: .min,
+        spacing: 4,
+        children: [
+          CircleAvatar(child: icon),
+          Text(title),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SizedBox(
+      height: 80,
+      child: Card(
+        elevation: 0,
+        child: Row(
+          crossAxisAlignment: .center,
+          mainAxisAlignment: .spaceEvenly,
+          children: [
+            _createQuickActionButton(icon: Icon(Icons.monitor), title: 'Menu 1'),
+            _createQuickActionButton(icon: Icon(Icons.monitor), title: 'Menu 2'),
+            _createQuickActionButton(icon: Icon(Icons.monitor), title: 'Menu 3'),
+            _createQuickActionButton(icon: Icon(Icons.monitor), title: 'Menu 4'),
+            _createQuickActionButton(icon: Icon(Icons.monitor), title: 'Menu 5'),
+          ],
+        ),
+      ),
+    );
   }
 }
